d261c9f7748741b3833802bf38172d2745da3b85
AA <Student@CAB4-2>
2024-11-05 16:20:20 +1000

    jknkj

---
+++ project_otchislenie_API/Controllers/StudentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project_otchislenie_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+    }
+}
---
+++ project_otchislenie_API/Models/ResignationLetter.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_otchislenie_API.Models
+{
+    public class ResignationLetter
+    {
+        public int Id { get; set; }
+        public string Reason { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public int StudentId { get; set; }
+        //[NotMapped]
+        //public Student Student { get; private set; }
+    }
+}
